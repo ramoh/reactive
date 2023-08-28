7b07c57c44a8df3cc8563cb4e2b12edd9cb8c101
Rajesh Mohanty <rajesh.mohanty@oracle.com>
2023-08-28 11:27:48 +0530

    task resource

---
+++ src/main/java/com/example/fullstack/task/TaskResource.java
@@ -0,0 +1,60 @@
+package com.example.fullstack.task;
+
+import java.util.List;
+
+import org.jboss.resteasy.reactive.ResponseStatus;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/v1/tasks")
+public class TaskResource {
+
+  private final TaskService taskService;
+
+  @Inject
+  public TaskResource(TaskService taskService) {
+    this.taskService = taskService;
+  }
+
+  @GET
+  public Uni<List<Task>> get() {
+    return taskService.listForUser();
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @ResponseStatus(201)
+  public Uni<Task> create(Task task) {
+    return taskService.create(task);
+  }
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Path("/{id}")
+  public Uni<Task> update(@PathParam("id") long id, Task task) {
+    task.id = id;
+    return taskService.update(task);
+  }
+
+  @DELETE
+  @Path("/{id}")
+  public Uni<Void> delete(@PathParam("id") long id) {
+    return taskService.delete(id);
+  }
+
+  @PUT
+  @Path("{id}/complete")
+  public Uni<Boolean> setComplete(long id, boolean complete) {
+    return taskService.setComplete(id, complete);
+  }
+
+}
