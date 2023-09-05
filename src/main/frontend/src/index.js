11af2a1398d936e1c3ecf5da972ba50e3e97b80f
Rajesh Mohanty <rajesh.mohanty@oracle.com>
2023-09-05 18:26:47 +0530

    front end layout

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { store } from './store';
+import { theme } from './styles/theme';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/main/frontend/src/styles/theme.js
@@ -0,0 +1,7 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  layout: {
+    drawerWidht: 240
+  }
+});
