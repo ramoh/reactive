c0cea2fd26954bdf96f994237662c2b78733945e
Rajesh Mohanty <rajesh.mohanty@oracle.com>
2023-09-13 20:22:25 +0530

    taskbar ui refactor

--- src/main/frontend/src/tasks/CompleteChip.js
+++ src/main/frontend/src/tasks/CompleteChip.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Chip } from "@mui/material";
 import CheckIcon from "@mui/icons-material/Check";
 
-export const CompleteChip = (task) => Boolean(task.complete) && (
-  <Chip icon={<CheckIcon />}
-    color="success"
-    label={new Date(task.complete).toLocaleDateString()}
-    variant="outlined"
+export const CompleteChip = ({ task }) => Boolean(task?.complete) && (
+  <Chip
+    icon={<CheckIcon />}
+    color='success'
+    label={new Date(task.complete).toLocaleDateString()} variant='outlined'
   />
 );
